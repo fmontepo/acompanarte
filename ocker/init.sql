436311f703ffa33f60118da06c6467160287b52c
fmontepo <fmontepo@gmail.com>
2026-04-11 13:05:52 -0300

    fix: eliminar carpeta ocker incorrecta

--- ocker/init.sql
+++
@@ -1,5 +0,0 @@
--- docker/init.sql
--- Este script se ejecuta automáticamente cuando PostgreSQL crea la DB por primera vez
--- Activa la extensión pgvector necesaria para los embeddings RAG
-
-CREATE EXTENSION IF NOT EXISTS vector;
